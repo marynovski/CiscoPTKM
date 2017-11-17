9b584f6edb0fe9e25be23c14fc68839f29c7ff36
Kamil Marynowski <marynovski@gmail.com>
2017-11-17 15:26:20 +0100

    Update connect.php
    Nieistotna zmiana...

--- connect.php
+++ connect.php
@@ -1,8 +1,30 @@
-<?php
-
-	$host = "mysql.hostinger.pl";
-	$db_user ="u263772236_pliki";
-	$db_password ="M@rynowski1@3";
-	$db_name = "u263772236_pliki";		//$host="localhost";		//$db_user = "root";		//$db_password = "";		//$db_name="stronowa";
-	
+<?php
+
+
+
+
+
+	$host = "...";
+
+
+	$db_user ="...";
+
+
+	$db_password ="...";
+
+
+	$db_name = "...";
+	
+	//$host="localhost";
+	
+	//$db_user = "root";
+	
+	//$db_password = "";
+	
+	//$db_name="stronowa";
+
+
+	
+
+
 ?>
